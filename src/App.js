90bb931962356c97525dd51fd20d21cd15bccf39
shylajabr <shylaja.b.r@gmail.com>
2021-06-16 00:21:17 +0200

    update the link to github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href=" "> shyla </a> and is{" "}
+        <a href="https://github.com/shylajabr/weather-app" target="-blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
