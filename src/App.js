7b7e17f726eabe59826a42a964a0938ff59e3ae7
shylajabr <shylaja.b.r@gmail.com>
2021-06-17 00:18:36 +0200

    update the bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by <a href=" "> shyla </a> and is{" "}
-        <a href="https://github.com/shylajabr/weather-app" target="-blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was coded by <a href=" "> shyla </a> and is{" "}
+          <a href="https://github.com/shylajabr/weather-app" target="-blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
